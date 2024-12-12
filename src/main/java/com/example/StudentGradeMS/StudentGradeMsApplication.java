a36a357b3cbc740e07032d11e1d62dba4a6d93f4
sarahtct <ssarahesha26@gmail.com>
2024-12-12 15:08:05 +0800

    initial commit

---
+++ src/main/java/com/example/StudentGradeMS/StudentGradeMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudentGradeMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentGradeMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentGradeMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StudentGradeMS/StudentGradeMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudentGradeMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentGradeMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
