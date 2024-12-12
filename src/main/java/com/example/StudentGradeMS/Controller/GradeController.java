a22f129ccfe276ed6bf6b01cdd7f9d17be629b7b
sarahtct <ssarahesha26@gmail.com>
2024-12-12 22:31:01 +0800

    UI CRUD for Grade

--- src/main/java/com/example/StudentGradeMS/Controller/GradeController.java
+++ src/main/java/com/example/StudentGradeMS/Controller/GradeController.java
@@ -1,4 +1,18 @@
 package com.example.StudentGradeMS.Controller;
 
+import com.example.StudentGradeMS.Model.Grade;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
 public class GradeController {
+    //testing purpose only
+    @GetMapping("/grade/add")
+    public String AddGrade(Model model) {
+        return "Grade/Add";
+    }
 }
