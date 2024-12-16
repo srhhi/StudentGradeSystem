24adc8ffb363ada702c8019db5f2082d99c42239
hakimin <m.hakiminazli@gmail.com>
2024-12-16 22:23:32 +0800

    Add ValidationException

---
+++ src/main/java/com/example/StudentGradeMS/Service/Service/ValidationException.java
@@ -0,0 +1,7 @@
+package com.example.StudentGradeMS.Service.Service;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
