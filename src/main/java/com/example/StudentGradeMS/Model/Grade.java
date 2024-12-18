1c1c92c43efb65788be48c6b77ed4a363423e535
sarahtct <ssarahesha26@gmail.com>
2024-12-18 22:43:29 +0800

    update UI

--- src/main/java/com/example/StudentGradeMS/Model/Grade.java
+++ src/main/java/com/example/StudentGradeMS/Model/Grade.java
@@ -9,7 +9,7 @@ import jakarta.persistence.Id;
 public class Grade {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private char grade;
     private String status;
